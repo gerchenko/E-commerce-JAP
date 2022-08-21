089f7ae3012a1200e52cddfce098dbf23e8f0f0d
gerchenko <k4ly007@hotmail.com>
2022-08-20 22:37:38 -0300

    Ultimos cambios Entregable 1

--- js/login.js
+++ js/login.js
@@ -1,3 +1,5 @@
+
+
 const form = document.querySelector("form")
 const email = document.getElementById("email")
 const password = document.getElementById("password")
@@ -27,4 +29,20 @@ window.response = (response) => {
     window.location.href = "https://gerchenko.github.io/E-commerce-JAP/";
 }
 
+function login(){
+    let usuario = {}
+    usuario.email = document.getElementById("email").value;
+    usuario.password = document.getElementById("password").value;
+    if(usuario.email != "" && usuario.password != ""){
+        localStorage.setItem("item", JSON.stringify(usuario));
+        location.href = "index.html";
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () =>{
+    document.getElementById("form").addEventListener("submit", (e)=>{
+        e.preventDefault();
+        login();
+    })
+})
 
